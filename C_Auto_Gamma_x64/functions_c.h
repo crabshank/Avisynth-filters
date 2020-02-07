a07bea3974eede63c0d25fb2bc0432e45c63900d
crabshank <44329154+crabshank@users.noreply.github.com>
2020-02-07 01:49:02 +0000

    Update functions_c.h

--- C_Auto_Gamma_x64/functions_c.h
+++ C_Auto_Gamma_x64/functions_c.h
@@ -1,7 +1,32 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define lerp(a,b,t) ((1 - (t)) * (a) + (t) * (b) )
+#define rcptwoFiveFive 1.0/255.0
 
+inline double fastPrecisePow(double a, double b) {
+  // calculate approximation with fraction of the exponent
+  int e = (int) b;
+  union {
+    double d;
+    int x[2];
+  } u = { a };
+  u.x[1] = (int)((b - e) * (u.x[1] - 1072632447) + 1072632447);
+  u.x[0] = 0;
+
+  // exponentiation by squaring with the exponent's integer part
+  // double r = u.d makes everything much slower, not sure why
+  double r = 1.0;
+  while (e) {
+    if (e & 1) {
+      r *= a;
+    }
+    a *= a;
+    e >>= 1;
+  }
+
+  return r * u.d;
+}
+//Source: https://martin.ankerl.com/2012/01/25/optimized-approximative-fastPrecisePow-in-c-and-cpp/
 
 
 void f_gammaLow(double rgb[3], double gamma,double gamma_hi, double dist){
@@ -9,10 +34,9 @@ void f_gammaLow(double rgb[3], double gamma,double gamma_hi, double dist){
 
     for (int i=0; i<3; i++){
     double col=rgb[i];
-    double ln=(col-(1-col)*pow(col,gamma));
+    double ln=(col-(1-col)*fastPrecisePow(col,gamma));
 gamma_hi=(ln<=0 || (col==0))?gamma:log(ln)/log(col);
-outcol[i]=1-fabs((pow(-col,gamma+1)+pow(col,gamma_hi+1)+pow(col,gamma))-rgb[i]);
-
+outcol[i]=1-fabs((fastPrecisePow(-col,gamma+1)+fastPrecisePow(col,gamma_hi+1)+fastPrecisePow(col,gamma))-rgb[i]);
 
     }
 
@@ -23,5 +47,8 @@ outcol[i]=1-fabs((pow(-col,gamma+1)+pow(col,gamma_hi+1)+pow(col,gamma))-rgb[i]);
 		outcol[2] = shift + outcol[2];
 
  dist=(rgb[0]-outcol[0])+(rgb[1]-outcol[1])+((outcol[2]-rgb[2]));
-}
 
+       double  mxOG=MAX(rgb[0],MAX(rgb[1],rgb[2]));
+       double  mx=MAX(outcol[0],MAX(outcol[1],outcol[2]));
+ dist=(mx>mxOG)?dist+(mx-mxOG):dist;
+}
