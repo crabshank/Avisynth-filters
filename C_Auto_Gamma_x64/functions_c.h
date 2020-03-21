e84a8dd8764a57b3225cb2adc6c24d5070a8de3a
crabshank <44329154+crabshank@users.noreply.github.com>
2020-03-21 00:58:16 +0000

    Update functions_c.h

--- C_Auto_Gamma_x64/functions_c.h
+++ C_Auto_Gamma_x64/functions_c.h
@@ -29,33 +29,19 @@ inline double fastPrecisePow(double a, double b) {
 //Source: https://martin.ankerl.com/2012/01/25/optimized-approximative-fastPrecisePow-in-c-and-cpp/
 
 
+
 void f_gammaLow(double rgb[3], double gamma,double gamma_hi, double dist){
     double outcol[3];
-
+double avg=(rgb[0]+rgb[1]+rgb[2])/3.0;
     for (int i=0; i<3; i++){
     double col=rgb[i];
     double ln=(col-(1-col)*fastPrecisePow(col,gamma));
 gamma_hi=(ln<=0 || (col==0))?gamma:log(ln)/log(col);
 outcol[i]=1-fabs((fastPrecisePow(-col,gamma+1)+fastPrecisePow(col,gamma_hi+1)+fastPrecisePow(col,gamma))-rgb[i]);
 
+
     }
 
-    	double shift = 1 - MIN(outcol[0], MIN(outcol[1], outcol[2])) - MAX(outcol[0], MAX(outcol[1], outcol[2]));
 
-		outcol[0] = shift + outcol[0];
-		outcol[1] = shift + outcol[1];
-		outcol[2] = shift + outcol[2];
-
-		double tot=outcol[0]+outcol[1]+outcol[2];
-		double totOG=rgb[0]+rgb[1]+rgb[2];
- dist=tot-totOG;
-
-       double  mxOG=MAX(rgb[0],MAX(rgb[1],rgb[2]));
-       double  mx=MAX(outcol[0],MAX(outcol[1],outcol[2]));
-
-       if(mx>mxOG){
-            double diff=mx-mxOG;
- dist=(dist<0)?dist-diff:dist+diff;
-       }
-
+ dist=(avg-outcol[0])+(avg-outcol[1])+((outcol[2]-avg));
 }
