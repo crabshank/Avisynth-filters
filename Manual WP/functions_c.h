706d889174946a94b40b2d5edd63564e4fbeb793
crabshank <44329154+crabshank@users.noreply.github.com>
2020-04-05 00:06:04 +0100

    Update functions_c.h

--- Manual WP/functions_c.h
+++ Manual WP/functions_c.h
@@ -27,47 +27,16 @@ inline double fastPrecisePow(double a, double b) {
 }
 //Source: https://martin.ankerl.com/2012/01/25/optimized-approximative-fastPrecisePow-in-c-and-cpp/
 
-
-void zeroMatrix( int rows_a, int cols_a, void* ma){
-
-      double (*a)[cols_a] = (double (*)[cols_a]) ma;
-
-	int i, j;
-
-	// Initializing elements of matrix mult to 0.
-	for(i = 0; i < rows_a; ++i)
-	{
-		for(j = 0; j < cols_a; ++j)
-		{
-			a[i][j] = 0;
-		}
-	}
-
-}
-
-void zeroArray( int cols_a, double a[]){
-
-	int i;
-
-	// Initializing elements of matrix mult to 0.
-	for(i = 0; i < cols_a; ++i)
-	{
-			a[i] = 0.0;
-	}
-}
-
   // A is m x p and B is p x n, so C is m x n
 void mul( int rows_a, int cols_a, int cols_b,void* ma, void* mb, void* mres)
 {
-
-
 
 int i,j,k;
 
       double (*a)[cols_a] = (double (*)[cols_a]) ma;
       double (*b)[cols_b] = (double (*)[cols_b]) mb;
       double (*res)[cols_b] = (double (*)[cols_b]) mres;
-  zeroMatrix( rows_a,cols_b,res);
+
       double *pta = &a[0][0];
       double *ptb = &b[0][0];
       double *ptr = &res[0][0];
@@ -80,6 +49,7 @@ int i,j,k;
 	{
 		for(j = 0; j < cols_b; j++)
 		{
+		    *(ptr+(i*cols_b+j))=0;
 			for(k=0; k<cols_a; k++)
 			{
 				*(ptr+(i*cols_b+j)) += *(pta+(i*cols_a+k))**(ptb+(k*cols_b+j));
@@ -128,7 +98,6 @@ CR[2][2]=BradTo[2]/BradFrom[2];
 double convBrad2[3][3]={{0,0,0},{0,0,0},{0,0,0}};
 double convBrad[3][3]={{0,0,0},{0,0,0},{0,0,0}};
 
-
 mul(3,3,3,BradfordInv,CR,convBrad2);
 mul(3,3,3,convBrad2,Bradford,convBrad);
 
@@ -140,75 +109,6 @@ mul(3,3,1,convBrad,XYZ,outp);
 //mul(3,3,1,Bradford,XYZ,outp); //WORKS (3x3 X double3)!!!
 
 }
-
-void rgb2xyY(double rgb[3],double outp[3]){
-
-
-      double  n1=(rgb[0] > 0.0404482362771082 )?fastPrecisePow(fabs((rgb[0]+0.055)/1.055),2.4):rgb[0]/12.92;
-     double   n2=(rgb[1] > 0.0404482362771082 )?fastPrecisePow(fabs((rgb[1]+0.055)/1.055),2.4):rgb[1]/12.92;
-      double  n3=(rgb[2] > 0.0404482362771082 )?fastPrecisePow(fabs((rgb[2]+0.055)/1.055),2.4):rgb[2]/12.92;
-
-
-
-double X=0.4124564*n1+0.3575761*n2+0.1804375*n3;
-double Y=0.2126729*n1+0.7151522*n2+0.072175*n3;
-double Z=0.0193339*n1+0.119192*n2+0.9503041*n3;
-
-	double XYZtot=X+Y+Z;
-
-	outp[0]=X/XYZtot;
-	outp[1]=Y/XYZtot;
-	outp[2]=Y;
-
-}
-
-//Source: https://stackoverflow.com/a/45263428; http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
-
-
-void xyY2XYZ(double xyY[3],double outp[3]){
-	double X=(xyY[2]/xyY[1])*xyY[0];
-	double Z=(xyY[2]/xyY[1])*(1-xyY[0]-xyY[1]);
-
-	outp[0]=X;
-	outp[1]=xyY[2];
-	outp[2]=Z;
-}
-
-void XYZ2xyY(double XYZ[3],double outp[3]){
-	double XYZtot=XYZ[0]+XYZ[1]+XYZ[2];
-
-	double x=XYZ[0]/XYZtot;
-	double y=XYZ[1]/XYZtot;
-
-    outp[0]=x;
-	outp[1]=y;
-	outp[2]=XYZ[1];
-}
-
-
-void xyY2rgb(double xyY[3],double RGB[3]){
-
- double Y=xyY[2];
-double  y=xyY[1];
-double x=xyY[0];
- double X=(Y/y)*x;
- double Z=(Y/y)*(1.0-x-y);
-
-double r=3.2404542*X-1.5371385*Y-0.4985314*Z;
-double g=-0.969266*X+1.8760108*Y+0.041556*Z;
-double b=0.0556434*X-0.2040259*Y+1.0572252*Z;
-
-
-   r=(r> 0.00313066844250063)?1.055 * fastPrecisePow(r,1.0/2.4) - 0.055:12.92 *r;
-   g=(g> 0.00313066844250063)?1.055 * fastPrecisePow(g,1.0/2.4) - 0.055:12.92 *g;
-   b=(b> 0.00313066844250063)?1.055 * fastPrecisePow(b,1.0/2.4) - 0.055:12.92 *b;
-
-RGB[0]=r;
-RGB[1]=g;
-RGB[2]=b;
-}
-
-//Source: http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
 
 void XYZ2rgb(double XYZ[3],double RGB[3]){
 
