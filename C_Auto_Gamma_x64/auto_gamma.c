e27eb98d57112428ad9851340586233581e8a053
crabshank <44329154+crabshank@users.noreply.github.com>
2020-06-29 04:11:50 +0100

    Update auto_gamma.c

--- C_Auto_Gamma_x64/auto_gamma.c
+++ C_Auto_Gamma_x64/auto_gamma.c
@@ -8,7 +8,9 @@ typedef struct Auto_Gamma {
       double bracketA;
       double bracketB;
       int tolerance;
+      int crush;
       int limitedRange;
+      int linear;
 } Auto_Gamma;
 
 AVS_VideoFrame * AVSC_CC Auto_Gamma_get_frame (AVS_FilterInfo * p, int n)
@@ -18,14 +20,17 @@ AVS_VideoFrame * AVSC_CC Auto_Gamma_get_frame (AVS_FilterInfo * p, int n)
 
   src = avs_get_frame(p->child, n);
 
-    int row_size, height, src_pitch,x, y,max_iters,tol,tolr,opt,lmr;
+    int row_size, height, src_pitch,x, y,max_iters,tol,tolr,opt,lmr,crs,lnr;
    BYTE* srcp;
    double a,b,c,mx,runTot_r,runTot_g,runTot_b,bOG,gOG,rOG,f_c,gamma_high,gamma_high_tmp,gamma_low,f_a,R,G,B,counter;
 
 a =   params->bracketA;
 b=params->bracketB;
 tol=params->tolerance;
+crs=params->crush;
+crs=round(crs);
 lmr=params->limitedRange;
+lnr=params->linear;
 
   avs_make_writable(p->env, &src);
 
@@ -54,6 +59,15 @@ bOG=currBlue*rcptwoFiveFive;     // B
        gOG=currGreen*rcptwoFiveFive;   //G
          rOG=currRed*rcptwoFiveFive;     // R
 
+         if (lnr==1){
+            bOG=(bOG > 0.0404482362771082)?fastPrecisePow(fabs((bOG+0.055)/1.055),2.4):bOG/12.92;
+            gOG=(gOG > 0.0404482362771082)?fastPrecisePow(fabs((gOG+0.055)/1.055),2.4):gOG/12.92;
+            rOG=(rOG > 0.0404482362771082)?fastPrecisePow(fabs((rOG+0.055)/1.055),2.4):rOG/12.92;
+         }else if(lnr==2){
+            bOG=(bOG > 0.00313066844250063)?1.055*(fastPrecisePow(bOG,rcptwo_four)) - 0.055:bOG*12.92;
+            gOG=(gOG > 0.00313066844250063)?1.055*(fastPrecisePow(gOG,rcptwo_four)) - 0.055:gOG*12.92;
+            rOG=(rOG > 0.00313066844250063)?1.055*(fastPrecisePow(rOG,rcptwo_four)) - 0.055:rOG*12.92;
+         }
 
 runTot_r+=rOG*0.2126729;
 runTot_g+=gOG*0.7151522;
@@ -108,10 +122,24 @@ double x_shift=(double)x/(double)row_size;
 bOG=currBlue*rcptwoFiveFive;     // B
        gOG=currGreen*rcptwoFiveFive;   //G
          rOG=currRed*rcptwoFiveFive;     // R
+
+           double bLin=(bOG > 0.0404482362771082)?fastPrecisePow(fabs((bOG+0.055)/1.055),2.4):bOG/12.92;
+           double gLin=(gOG > 0.0404482362771082)?fastPrecisePow(fabs((gOG+0.055)/1.055),2.4):gOG/12.92;
+           double rLin=(rOG > 0.0404482362771082)?fastPrecisePow(fabs((rOG+0.055)/1.055),2.4):rOG/12.92;
+
+                  if (lnr==1){
+                        bOG=bLin;
+                        gOG=gLin;
+                        rOG=rLin;
+         }else if(lnr==2){
+            bOG=(bOG > 0.00313066844250063)?1.055*(fastPrecisePow(bOG,rcptwo_four)) - 0.055:bOG*12.92;
+            gOG=(gOG > 0.00313066844250063)?1.055*(fastPrecisePow(gOG,rcptwo_four)) - 0.055:gOG*12.92;
+            rOG=(rOG > 0.00313066844250063)?1.055*(fastPrecisePow(rOG,rcptwo_four)) - 0.055:rOG*12.92;
+         }
 R=rOG;
 G=gOG;
 B=bOG;
-double Yog=0.2126729*rOG+0.7151522*gOG+0.072175*bOG;
+double Yog=0.2126729*rLin+0.7151522*gLin+0.072175*bLin;
 
          if(opt==1){
 
@@ -120,13 +148,19 @@ G=lerp(fastPrecisePow(gOG,gamma_low),fastPrecisePow(gOG,gamma_high),gOG);
 B=lerp(fastPrecisePow(bOG,gamma_low),fastPrecisePow(bOG,gamma_high),bOG);
 
 double Ynw=0.2126729*R+0.7151522*G+0.072175*B;
-
+if (crs==0){
 if (Ynw<Yog){
 R=rOG;
 G=gOG;
 B=bOG;
 }
-
+}else if (crs==1){
+if (Ynw>Yog){
+R=rOG;
+G=gOG;
+B=bOG;
+}
+}
 
 if(lmr==1){
     R=(( R*255*(235-16)*rcptwoFiveFive )+16)*rcptwoFiveFive;
@@ -163,8 +197,9 @@ if (!params)
         params->bracketA = avs_defined(avs_array_elt(args, 1))?avs_as_float(avs_array_elt(args, 1)):0;
         params->bracketB = avs_defined(avs_array_elt(args, 2))?avs_as_float(avs_array_elt(args, 2)):11;
         params->tolerance = avs_defined(avs_array_elt(args, 3))?avs_as_int(avs_array_elt(args, 3)):3    ;
-        params->limitedRange= avs_defined(avs_array_elt(args, 4))?avs_as_bool(avs_array_elt(args, 4)):false;
-
+        params->crush = avs_defined(avs_array_elt(args, 4))?avs_as_int(avs_array_elt(args, 4)):0;
+        params->limitedRange= avs_defined(avs_array_elt(args, 5))?avs_as_bool(avs_array_elt(args, 5)):false;
+        params->linear= avs_defined(avs_array_elt(args, 6))?avs_as_int(avs_array_elt(args, 6)):0;
 
   if (!avs_is_rgb32(&fi->vi)) {
     return avs_new_value_error ("Input video must be in RGB format!");
@@ -181,6 +216,6 @@ if (!params)
 
 const char * AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment * env)
 {
-   avs_add_function(env, "Auto_Gamma", "c[a]f[b]f[tolerance]i[limitedRange]b", create_Auto_Gamma, 0);
+   avs_add_function(env, "Auto_Gamma", "c[a]f[b]f[tolerance]i[crush]i[limitedRange]b[linear]i", create_Auto_Gamma, 0);
    return "Auto_Gamma sample C plugin";
 }
