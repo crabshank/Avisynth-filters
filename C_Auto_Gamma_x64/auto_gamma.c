996e8646d30619cc8152c6854e44f4d7b0001109
crabshank <44329154+crabshank@users.noreply.github.com>
2020-07-01 23:52:43 +0100

    Update auto_gamma.c

--- C_Auto_Gamma_x64/auto_gamma.c
+++ C_Auto_Gamma_x64/auto_gamma.c
@@ -11,6 +11,8 @@ typedef struct Auto_Gamma {
       int crush;
       int limitedRange;
       int linear;
+      int Rec2020;
+      int sixtyFour;
 } Auto_Gamma;
 
 AVS_VideoFrame * AVSC_CC Auto_Gamma_get_frame (AVS_FilterInfo * p, int n)
@@ -20,9 +22,9 @@ AVS_VideoFrame * AVSC_CC Auto_Gamma_get_frame (AVS_FilterInfo * p, int n)
 
   src = avs_get_frame(p->child, n);
 
-    int row_size, height, src_pitch,x, y,max_iters,tol,tolr,opt,lmr,crs,lnr;
+    int row_size, height, src_pitch,x, y,max_iters,tol,tolr,opt,lmr,crs,lnr,rec,sxf;
    BYTE* srcp;
-   double a,b,c,mx,runTot_r,runTot_g,runTot_b,bOG,gOG,rOG,f_c,gamma_high,gamma_high_tmp,gamma_low,f_a,R,G,B,counter;
+   double a,b,c,mx,runTot_r,runTot_g,runTot_b,bOG,gOG,rOG,f_c,gamma_high,gamma_high_tmp,gamma_low,f_a,R,G,B,counter,rLin,gLin,bLin;
 
 a =   params->bracketA;
 b=params->bracketB;
@@ -31,6 +33,10 @@ crs=params->crush;
 crs=round(crs);
 lmr=params->limitedRange;
 lnr=params->linear;
+rec=params->Rec2020;
+rec=round(rec);
+sxf=params->sixtyFour;
+sxf=round(sxf);
 
   avs_make_writable(p->env, &src);
 
@@ -51,23 +57,35 @@ counter=0;
 
 
 //double x_shift=(double)x/(double)row_size;
-                 double currBlue=(double)srcp[x];
-                double currGreen=(double)srcp[x+1];
-                double currRed=(double)srcp[x+2];
+                 double currBlue=(sxf==1)?(double)srcp[x]+srcp[x+1]*256:(double)srcp[x];
+                 double currGreen=(sxf==1)?(double)srcp[x+2]+srcp[x+3]*256:(double)srcp[x+1];
+                 double currRed=(sxf==1)?(double)srcp[x+4]+srcp[x+5]*256:(double)srcp[x+2];
 
-bOG=currBlue*rcptwoFiveFive;     // B
-       gOG=currGreen*rcptwoFiveFive;   //G
-         rOG=currRed*rcptwoFiveFive;     // R
 
-         if (lnr!=2){
-            bOG=(bOG > 0.0404482362771082)?fastPrecisePow(fabs((bOG+0.055)/1.055),2.4):bOG/12.92;
-            gOG=(gOG > 0.0404482362771082)?fastPrecisePow(fabs((gOG+0.055)/1.055),2.4):gOG/12.92;
-            rOG=(rOG > 0.0404482362771082)?fastPrecisePow(fabs((rOG+0.055)/1.055),2.4):rOG/12.92;
-         }
+    bOG=(sxf==1)?currBlue*rcptHiBit:currBlue*rcptwoFiveFive;     // B
+       gOG=(sxf==1)?currGreen*rcptHiBit:currGreen*rcptwoFiveFive;   //G
+         rOG=(sxf==1)?currRed*rcptHiBit:currRed*rcptwoFiveFive;     // R
 
+    if (lnr!=2){
+        if (rec==1){
+           bOG=(bOG < recBetaLin )?rcpFourFive*bOG:fastPrecisePow(-1*(rcpRecAlpha*(1-recAlpha-bOG)),rcpTxFourFive);
+           gOG=(gOG < recBetaLin )?rcpFourFive*gOG:fastPrecisePow(-1*(rcpRecAlpha*(1-recAlpha-gOG)),rcpTxFourFive);
+           rOG=(rOG < recBetaLin )?rcpFourFive*rOG:fastPrecisePow(-1*(rcpRecAlpha*(1-recAlpha-rOG)),rcpTxFourFive);
+        }else{
+                bOG=(bOG > 0.0404482362771082 )?fastPrecisePow(fabs((bOG+0.055)*rcpOFiveFive),2.4):bOG*rcpTwelveNineTwo;
+                gOG=(gOG > 0.0404482362771082 )?fastPrecisePow(fabs((gOG+0.055)*rcpOFiveFive),2.4):gOG*rcpTwelveNineTwo;
+                rOG=(rOG > 0.0404482362771082 )?fastPrecisePow(fabs((rOG+0.055)*rcpOFiveFive),2.4):rOG*rcpTwelveNineTwo;
+            }
+    }
+  if (rec==1){
+    runTot_r+=rOG*0.26272171736164;
+runTot_g+=gOG*0.677989275502262;
+runTot_b+=bOG*0.059289007136098;
+  }else{
 runTot_r+=rOG*0.2126729;
 runTot_g+=gOG*0.7151522;
 runTot_b+=bOG*0.072175;
+  }
 counter+=1;
 
         x=x+3;
@@ -109,33 +127,49 @@ while(p<=max_iters){
       for (y=0; y<height; y++) {
       for (x=0; x<row_size; x++) {
 
-double x_shift=(double)x/(double)row_size;
+//double x_shift=(double)x/(double)row_size;
 
-                 double currBlue=(double)srcp[x];
-                double currGreen=(double)srcp[x+1];
-                double currRed=(double)srcp[x+2];
+                 double currBlue=(sxf==1)?(double)srcp[x]+srcp[x+1]*256:(double)srcp[x];
+                 double currGreen=(sxf==1)?(double)srcp[x+2]+srcp[x+3]*256:(double)srcp[x+1];
+                 double currRed=(sxf==1)?(double)srcp[x+4]+srcp[x+5]*256:(double)srcp[x+2];
 
-bOG=currBlue*rcptwoFiveFive;     // B
-       gOG=currGreen*rcptwoFiveFive;   //G
-         rOG=currRed*rcptwoFiveFive;     // R
 
-           double bLin=(bOG > 0.0404482362771082)?fastPrecisePow(fabs((bOG+0.055)/1.055),2.4):bOG/12.92;
-           double gLin=(gOG > 0.0404482362771082)?fastPrecisePow(fabs((gOG+0.055)/1.055),2.4):gOG/12.92;
-           double rLin=(rOG > 0.0404482362771082)?fastPrecisePow(fabs((rOG+0.055)/1.055),2.4):rOG/12.92;
+    bOG=(sxf==1)?currBlue*rcptHiBit:currBlue*rcptwoFiveFive;     // B
+       gOG=(sxf==1)?currGreen*rcptHiBit:currGreen*rcptwoFiveFive;   //G
+         rOG=(sxf==1)?currRed*rcptHiBit:currRed*rcptwoFiveFive;     // R
+
+        if (rec==1){
+           bLin=(bOG < recBetaLin )?rcpFourFive*bOG:fastPrecisePow(-1*(rcpRecAlpha*(1-recAlpha-bOG)),rcpTxFourFive);
+           gLin=(gOG < recBetaLin )?rcpFourFive*gOG:fastPrecisePow(-1*(rcpRecAlpha*(1-recAlpha-gOG)),rcpTxFourFive);
+           rLin=(rOG < recBetaLin )?rcpFourFive*rOG:fastPrecisePow(-1*(rcpRecAlpha*(1-recAlpha-rOG)),rcpTxFourFive);
+        }else{
+                bLin=(bOG > 0.0404482362771082 )?fastPrecisePow(fabs((bOG+0.055)*rcpOFiveFive),2.4):bOG*rcpTwelveNineTwo;
+                gLin=(gOG > 0.0404482362771082 )?fastPrecisePow(fabs((gOG+0.055)*rcpOFiveFive),2.4):gOG*rcpTwelveNineTwo;
+                rLin=(rOG > 0.0404482362771082 )?fastPrecisePow(fabs((rOG+0.055)*rcpOFiveFive),2.4):rOG*rcpTwelveNineTwo;
+            }
 
                   if (lnr==1){
                         bOG=bLin;
                         gOG=gLin;
                         rOG=rLin;
          }else if(lnr==2){
-            bOG=(bOG > 0.00313066844250063)?1.055*(fastPrecisePow(bOG,rcptwo_four)) - 0.055:bOG*12.92;
-            gOG=(gOG > 0.00313066844250063)?1.055*(fastPrecisePow(gOG,rcptwo_four)) - 0.055:gOG*12.92;
-            rOG=(rOG > 0.00313066844250063)?1.055*(fastPrecisePow(rOG,rcptwo_four)) - 0.055:rOG*12.92;
+
+                     if (rec==1){
+    rOG=(rOG< recBeta)?4.5*rOG:recAlpha*fastPrecisePow(rOG,0.45)-(recAlpha-1);
+    rOG=(gOG< recBeta)?4.5*gOG:recAlpha*fastPrecisePow(gOG,0.45)-(recAlpha-1);
+    rOG=(bOG< recBeta)?4.5*bOG:recAlpha*fastPrecisePow(bOG,0.45)-(recAlpha-1);
+        }else{
+ rOG=(rOG> 0.00313066844250063)?1.055 * fastPrecisePow(rOG,rcpTwoFour) - 0.055:12.92 *rOG;
+ bOG=(gOG> 0.00313066844250063)?1.055 * fastPrecisePow(gOG,rcpTwoFour) - 0.055:12.92 *gOG;
+ gOG=(bOG> 0.00313066844250063)?1.055 * fastPrecisePow(bOG,rcpTwoFour) - 0.055:12.92 *bOG;
+
+            }
+
          }
 R=rOG;
 G=gOG;
 B=bOG;
-double Yog=0.2126729*rLin+0.7151522*gLin+0.072175*bLin;
+double Yog=(rec==1)?rLin*0.26272171736164+gLin*0.677989275502262+bLin*0.059289007136098:0.2126729*rLin+0.7151522*gLin+0.072175*bLin;
 
          if(opt==1){
 
@@ -143,7 +177,7 @@ R=lerp(fastPrecisePow(rOG,gamma_low),fastPrecisePow(rOG,gamma_high),rOG);
 G=lerp(fastPrecisePow(gOG,gamma_low),fastPrecisePow(gOG,gamma_high),gOG);
 B=lerp(fastPrecisePow(bOG,gamma_low),fastPrecisePow(bOG,gamma_high),bOG);
 
-double Ynw=0.2126729*R+0.7151522*G+0.072175*B;
+double Ynw=(rec==1)?0.26272171736164*R+0.677989275502262*G+0.059289007136098*B:0.2126729*R+0.7151522*G+0.072175*B;
 if (crs==0){
 if (Ynw<Yog){
 R=rOG;
@@ -168,12 +202,20 @@ if(lmr==1){
 
 ////////////////////////////////////////////////////
 //if (x_shift>0.5){
-                srcp[x] = MAX(MIN(round(B*255),255),0);
-             srcp[x+1] =MAX(MIN(round(G*255),255),0);
-        srcp[x+2] = MAX(MIN(round(R*255),255),0);
-//}
+int wp_b=MAX(MIN(round(B*255),255),0);
+int wp_g=MAX(MIN(round(G*255),255),0);
+int wp_r=MAX(MIN(round(R*255),255),0);
 
-             x=x+3;
+srcp[x] =wp_b; //blue : blue
+srcp[x+1] =(sxf==1)?wp_b:wp_g; // blue : green
+srcp[x+2] =(sxf==1)? wp_g:wp_r; // green: red
+srcp[x+3] =(sxf==1)? wp_g:srcp[x+3]; //green : self
+srcp[x+4] =(sxf==1)? wp_r:srcp[x+4]; //red : self
+srcp[x+5] =(sxf==1)? wp_r:srcp[x+5]; //red : self
+
+
+x=(sxf==1)?x+7:x+3;
+//}
       }
             srcp += src_pitch;
       }
@@ -196,10 +238,17 @@ if (!params)
         params->crush = avs_defined(avs_array_elt(args, 4))?avs_as_int(avs_array_elt(args, 4)):0;
         params->limitedRange= avs_defined(avs_array_elt(args, 5))?avs_as_bool(avs_array_elt(args, 5)):false;
         params->linear= avs_defined(avs_array_elt(args, 6))?avs_as_int(avs_array_elt(args, 6)):0;
+        params->Rec2020 = avs_defined(avs_array_elt(args, 7))?avs_as_bool(avs_array_elt(args, 7)):false;
 
-  if (!avs_is_rgb32(&fi->vi)) {
-    return avs_new_value_error ("Input video must be in RGB format!");
+  if (!((avs_is_rgb32(&fi->vi))||(avs_is_rgb64(&fi->vi)))) {
+    return avs_new_value_error ("Input video must be in RGB32 OR RGB64 format!");
   } else {
+
+     if(avs_defined(avs_array_elt(args, 8))){
+        params->sixtyFour =avs_as_bool(avs_array_elt(args, 8));
+     }else{
+       params->sixtyFour = (avs_is_rgb64(&fi->vi))?true:false;
+     }
          fi->user_data = (void*) params;
     fi->get_frame = Auto_Gamma_get_frame;
     v = avs_new_value_clip(new_clip);
@@ -212,6 +261,6 @@ if (!params)
 
 const char * AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment * env)
 {
-   avs_add_function(env, "Auto_Gamma", "c[a]f[b]f[tolerance]i[crush]i[limitedRange]b[linear]i", create_Auto_Gamma, 0);
+   avs_add_function(env, "Auto_Gamma", "c[a]f[b]f[tolerance]i[crush]i[limitedRange]b[linear]i[Rec2020]b[sixtyFour]b", create_Auto_Gamma, 0);
    return "Auto_Gamma sample C plugin";
 }
