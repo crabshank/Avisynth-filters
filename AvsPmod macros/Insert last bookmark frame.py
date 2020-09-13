7d8b557fd6a55dc5a710b353742a82eb7ef8b4f3
crabshank <44329154+crabshank@users.noreply.github.com>
2020-09-13 23:38:56 +0100

    Add files via upload

--- AvsPmod macros/Insert last bookmark frame.py
+++ AvsPmod macros/Insert last bookmark frame.py
@@ -1,7 +1,12 @@
 bookmarks = avsp.GetBookmarkList()
+curr=avsp.GetFrameNumber()
 if bookmarks:
-	bookmarks = list(set(bookmarks))
-	bookmarks.sort() 
-	avsp.InsertText('%i' % bookmarks[-1],pos=None)
+		bookmarks = list(set(bookmarks))
+		bookmarks.sort(reverse=True) 
+		for k in range(len(bookmarks)): 
+			if bookmarks[k] <= curr:
+				curr=bookmarks[k]
+				break
+		avsp.InsertText('%i' % 	curr,pos=None)
 else:
-	avsp.InsertText('%i' %  avsp.GetFrameNumber(),pos=None)
+	avsp.InsertText('%i' %  curr,pos=None)
