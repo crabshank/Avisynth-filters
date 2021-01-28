6d83455dc460e02c29be42f13ba48f20892b45ec
crabshank <44329154+crabshank@users.noreply.github.com>
2021-01-28 13:40:25 +0000

    Update functions_c.h

--- C_Linear_Gamma_x64/functions_c.h
+++ C_Linear_Gamma_x64/functions_c.h
@@ -47,21 +47,3 @@ inline double fastPrecisePow(double a, double b) {
   return r * u.d;
 }
 //Source: https://martin.ankerl.com/2012/01/25/optimized-approximative-fastPrecisePow-in-c-and-cpp/
-
-
-
-void f_gammaLow(double rgb[3], double gamma,double gamma_hi, double dist){
-    double outcol[3];
-double avg=(rgb[0]+rgb[1]+rgb[2])*third;
-    for (int i=0; i<3; i++){
-    double col=rgb[i];
-    double ln=(col-(1-col)*fastPrecisePow(col,gamma));
-gamma_hi=(ln<=0 || (col==0))?gamma:log(ln)/log(col);
-outcol[i]=1-fabs((fastPrecisePow(-col,gamma+1)+fastPrecisePow(col,gamma_hi+1)+fastPrecisePow(col,gamma))-rgb[i]);
-
-
-    }
-
-
- dist=(avg-outcol[0])+(avg-outcol[1])+((outcol[2]-avg));
-}
