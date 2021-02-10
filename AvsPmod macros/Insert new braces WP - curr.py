8b4cfe74a67c3edb8e929fae829fa27a14a18640
crabshank <44329154+crabshank@users.noreply.github.com>
2021-02-10 23:48:14 +0000

    Add files via upload

--- AvsPmod macros/Insert new braces WP - curr.py
+++ AvsPmod macros/Insert new braces WP - curr.py
@@ -15,7 +15,7 @@ for x in range(len(txl)):
 		y2s=y2[0].split(',')
 		if len(y2)>0:
 			p1=str(ys[0])+','+str(ys[1])+','+str(ys[2])+','+str(ys[3])+','+str(curr-1)+','+str(ys[5])+','+str(ys[6])          
-			p2='{-1, -1, -1 ,'+str(curr)+',0,0.312727,0.329023}'
+			p2='{ -1, -1, -1 ,'+str(curr)+',0,0.312727,0.329023}'
 			p3=txt.replace(y[0], p1+'\n'+p2)
 			avsp.SetText(p3)
 			break
