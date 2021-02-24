5f368a0c5464a513e61b315451d06e59d6327327
crabshank <44329154+crabshank@users.noreply.github.com>
2021-02-24 15:02:01 +0000

    Update (De)focus.py

--- AvsPmod macros/(De)focus.py
+++ AvsPmod macros/(De)focus.py
@@ -4,7 +4,7 @@ wnd = avsp.GetWindow()
 script = wnd.currentScript
 
 curr=avsp.GetFrameNumber()
-
+bk=curr
 txt=avsp.GetText(index=None, clean=False)
 txl=txt.splitlines()
 txl.reverse()
@@ -33,6 +33,29 @@ for x in range(len(txl)):
 		ff=int(y[0].split('=')[1].strip())
 		if swt==1:
 			avsp.SetText(txt.replace(y[0], 'global focus_frame = '+str(curr)))
+		break
+		
+
+bookmarks = avsp.GetBookmarkList()
+
+if bookmarks:
+		bookmarks = list(set(bookmarks))
+		bookmarks.sort(reverse=True) 
+		for k in range(len(bookmarks)): 
+			if bookmarks[k] <= curr:
+				bk=bookmarks[k] #curr := last bookmark if there are any
+				break
+
+txt=avsp.GetText(index=None, clean=False)
+txl=txt.splitlines()
+txl.reverse()
+
+for x in range(len(txl)):
+	y=re.findall('.*\s+last_bk\s*=\s*\d{1,}',txl[x])
+	if len(y)>0: #if find match
+		ff=int(y[0].split('=')[1].strip())
+		if swt==1:
+			avsp.SetText(txt.replace(y[0], 'global last_bk = '+str(bk)))
 		pos = script.GetSelectionEnd()
 		script.GotoPos(pos)
 		avsp.ShowVideoFrame(framenum=ff , index=None, forceRefresh=True)
