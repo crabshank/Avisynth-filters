9e6116bef6e8f94ba50b2facb1119cec5aeac258
crabshank <44329154+crabshank@users.noreply.github.com>
2022-01-02 05:54:26 +0000

    Update functions_c.h

--- C_Saturation_percentiles_x64/functions_c.h
+++ C_Saturation_percentiles_x64/functions_c.h
@@ -1,5 +1,6 @@
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define lerp(a,b,t) ((1 - (t)) * (a) + (t) * (b) )
 #define rcptwoFiveFive 1.0/255.0
 #define rcptHiBit 1.0/65535.0
 
