bcb61ca18e9330a1e87c212ddc2c8e99e802d96e
crabshank <44329154+crabshank@users.noreply.github.com>
2022-02-04 11:58:06 +0000

    Update saturation_percentiles.c

--- C_Saturation_percentiles_x64/saturation_percentiles.c
+++ C_Saturation_percentiles_x64/saturation_percentiles.c
@@ -6,6 +6,7 @@
 
 typedef struct Saturation_Percentiles {
     int below;
+    int out;
     int sixtyFour;
     int pxels;
     double *sats;
@@ -14,6 +15,7 @@ typedef struct Saturation_Percentiles {
     int refEl_hi;
     double refEl_lerp;
     int refEl;
+    int gOut;
 } Saturation_Percentiles;
 
 AVS_VideoFrame * AVSC_CC Saturation_Percentiles_get_frame (AVS_FilterInfo * p, int n)
@@ -23,14 +25,17 @@ AVS_VideoFrame * AVSC_CC Saturation_Percentiles_get_frame (AVS_FilterInfo * p, i
 
   src = avs_get_frame(p->child, n);
 
-    int row_size, height, src_pitch,x, y,sxf,blw;
+    int row_size, height, src_pitch,x, y,sxf,blw,gOt;
     long pxls;
   const BYTE* rrcp;
    BYTE* srcp;
-   double bOG,gOG,rOG;
+   double bOG,gOG,rOG,ot;
 
 sxf=params->sixtyFour;
 blw=params->below;
+ot=params->out;
+gOt=params->gOut;
+
 
   avs_make_writable(p->env, &src);
 
@@ -58,10 +63,27 @@ double* satsOG=params->satsOG;
 
 
              double mx=MAX(rOG,MAX(gOG,bOG));
-    double sat=(mx==0)?0:(mx-MIN(rOG,MIN(gOG,bOG)))/mx;
+             double mn=MIN(rOG,MIN(gOG,bOG));
+             double chr=mx-mn;
+             double sat;
+             double mtr=chr;
 
-         sats[k]=sat;
-         satsOG[k]=sat;
+                //out: 0->sat, 1->chr, 2->mcs
+
+             if(ot!=1){ // sat || mcs
+                sat=(mx==0)?0:(chr)/mx;
+
+                if(ot==2){ //mcs
+                    mtr=MIN(sat,chr);
+                }else{ //sat
+                    mtr=sat;
+                }
+
+             }
+
+
+         sats[k]=mtr;
+         satsOG[k]=mtr;
 
         k++;
         x=(sxf==1)?x+7:x+3;
@@ -80,21 +102,21 @@ k=0;
 
 if(blw==1){
 if(satsOG[k]>refSat){
-srcp[x] =0; //blue : blue
-srcp[x+1] =(sxf==1)?0:0; // blue : green
-srcp[x+2] =(sxf==1)? 0:0; // green: red
-srcp[x+3] =(sxf==1)? 0:srcp[x+3]; //green : self
-srcp[x+4] =(sxf==1)? 0:srcp[x+4]; //red : self
-srcp[x+5] =(sxf==1)? 0:srcp[x+5]; //red : self
+srcp[x] =gOt; //blue : blue
+srcp[x+1] =(sxf==1)?gOt:gOt; // blue : green
+srcp[x+2] =(sxf==1)? gOt:gOt; // green: red
+srcp[x+3] =(sxf==1)? gOt:srcp[x+3]; //green : self
+srcp[x+4] =(sxf==1)? gOt:srcp[x+4]; //red : self
+srcp[x+5] =(sxf==1)? gOt:srcp[x+5]; //red : self
 }
 }else{
 if(satsOG[k]<refSat){
-srcp[x] =0; //blue : blue
-srcp[x+1] =(sxf==1)?0:0; // blue : green
-srcp[x+2] =(sxf==1)? 0:0; // green: red
-srcp[x+3] =(sxf==1)? 0:srcp[x+3]; //green : self
-srcp[x+4] =(sxf==1)? 0:srcp[x+4]; //red : self
-srcp[x+5] =(sxf==1)? 0:srcp[x+5]; //red : self
+srcp[x] =gOt; //blue : blue
+srcp[x+1] =(sxf==1)?gOt:gOt; // blue : green
+srcp[x+2] =(sxf==1)? gOt:gOt; // green: red
+srcp[x+3] =(sxf==1)? gOt:srcp[x+3]; //green : self
+srcp[x+4] =(sxf==1)? gOt:srcp[x+4]; //red : self
+srcp[x+5] =(sxf==1)? gOt:srcp[x+5]; //red : self
 }
 }
 
@@ -128,16 +150,39 @@ if (!params)
 
        double perc= avs_defined(avs_array_elt(args, 1))?avs_as_float(avs_array_elt(args, 1)):1;
         params->below= avs_defined(avs_array_elt(args, 2))?avs_as_bool(avs_array_elt(args, 2)):true;
+        params->out= avs_defined(avs_array_elt(args, 3))?avs_as_int(avs_array_elt(args, 3)):0;
+        int col= avs_defined(avs_array_elt(args, 4))?avs_as_int(avs_array_elt(args, 4)):0;
+
+        double gOtD;
+
+        switch(col){
+            case 1:
+             gOtD=0.5;
+            break;
+
+            case 2:
+              gOtD=1;
+            break;
+
+            default:
+              gOtD=0;
+            break;
+        }
 
   if (!((avs_is_rgb32(&fi->vi))||(avs_is_rgb64(&fi->vi)))) {
     return avs_new_value_error ("Input video must be in RGB32 OR RGB64 format!");
+  }else if (params->out<0 || params->out>2) {
+    return avs_new_value_error ("\"out\" must be between 0 and 2!");
+  }else if (col<0 || col>2) {
+    return avs_new_value_error ("\"col\" must be between 0 and 2!");
   } else {
 
-     if(avs_defined(avs_array_elt(args, 3))){
-        params->sixtyFour =avs_as_bool(avs_array_elt(args, 3));
+     if(avs_defined(avs_array_elt(args, 5))){
+        params->sixtyFour =avs_as_bool(avs_array_elt(args, 5));
      }else{
        params->sixtyFour = (avs_is_rgb64(&fi->vi))?true:false;
      }
+     params->gOut=(params->sixtyFour==1)?round(gOtD*65535):round(gOtD*255);
      params->pxels=fi->vi.height*fi->vi.width;
      double d_pxels=(double)params->pxels;
     if(params->pxels==1){
@@ -164,6 +209,6 @@ if (!params)
 
 const char * AVSC_CC avisynth_c_plugin_init(AVS_ScriptEnvironment * env)
 {
-   avs_add_function(env, "Saturation_Percentiles", "c[perc]f[below]b[sixtyFour]b", create_Saturation_Percentiles, 0);
+   avs_add_function(env, "Saturation_Percentiles", "c[perc]f[below]b[out]i[col]i[sixtyFour]b", create_Saturation_Percentiles, 0);
    return "Saturation_Percentiles C plugin";
 }
